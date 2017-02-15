1db915bdce68287ceb8c3caf764357ed713e12b9
Karim Naguib <karim.naguib@evidenceaction.org>
2017-02-15 11:48:09 -0500

    New multi_spread function

--- R/util.R
+++ R/util.R
@@ -1,4 +1,4 @@
-#' Title
+#' Identify outlier values
 #'
 #' @param .values
 #'
@@ -8,4 +8,22 @@
 #' @examples
 is_outlier <- function(.values) {
   .values %>% { . < quantile(., 0.25) - 1.5 * IQR(.) | . > quantile(., 0.75) + 1.5 * IQR(.) }
+}
+
+#' Make data wide for multiple columns
+#'
+#' @param .data
+#' @param .grp.id
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+multi_spread <- function(.data, .grp.id, ...) {
+  .data %>%
+    tidyr::gather_("multi_spread_temp_key", "multi_spread_temp_val",
+                   gather_cols = unname(dplyr::select_vars(colnames(.), ...))) %>%
+    tidyr::unite_("multi_spread_temp", c("multi_spread_temp_key", .grp.id)) %>%
+    tidyr::spread(multi_spread_temp, multi_spread_temp_val)
 }
