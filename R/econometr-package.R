7909df75baec14092c1a3058c0f64889c50904b0
Karim Naguib <karim.naguib@evidenceaction.org>
2017-04-25 12:32:13 -0400

    import stats

--- R/econometr-package.R
+++ R/econometr-package.R
@@ -4,6 +4,7 @@
 #' @name econometr
 #' @import magrittr
 #' @import dplyr
+#' @impot stats
 #' @importFrom stats as.formula contrasts lm.fit model.frame model.matrix model.response na.omit
 #' @importFrom car contr.Treatment
 #'
